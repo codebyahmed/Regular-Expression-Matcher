0f463fe40ae64e68f1e18595fced6b5a61b1a8af
Ahmed Iqbal <ahmediqbal.dev@gmail.com>
2023-10-11 23:54:00 +0500

    Add comments

--- regular-expression-matcher.c
+++ regular-expression-matcher.c
@@ -1,3 +1,25 @@
+/*
+ * This program implements Rob Pike's Regular Expression Matcher with multi-position capabilities and extra meta-characters.
+ * The program reads two lines from a file specified by the user, removes the newline character if it exists,
+ * and performs regular expression matching on the two lines. The program prints the positions of the matches
+ * found in the second line. The regular expression syntax supported by the program includes:
+ * - '.' to match any single character
+ * - '*' to match zero or more occurrences of the previous character
+ * - '?' to match zero or one occurrence of the previous character
+ * - '+' to match one or more occurrences of the previous character
+ * - '[]' to match any character in the set
+ * - '^' to negate the set
+ * - '\' to escape special characters
+ * The program consists of the following functions:
+ * - match: search for regexp anywhere in text and print the positions
+ * - matchhere: search for regexp at the beginning of text
+ * - matchstar: search for c*regexp at the beginning of text
+ * - matchquestion: search for c?regexp at the beginning of text
+ * - matchset: search for a character in the set
+ * - matchplus: search for c+regexp at the beginning of text
+ * - readFromFile: read two lines from a file and remove the newline character if it exists
+ */
+
 #include <stdio.h>
 #include <string.h>
 
@@ -10,18 +32,15 @@ int matchset(char c, char *regexp, char *text);
 int matchplus(int c, char *regexp, char *text);
 
 // Function to read two lines from the file, removing the newline character if it exists
-int readFromFile(const char *filename, char *line1, size_t size1, char *line2, size_t size2)
-{
+int readFromFile(const char *filename, char *line1, size_t size1, char *line2, size_t size2) {
     FILE *fp = fopen(filename, "r");
-    if (fp == NULL)
-    {
+    if (fp == NULL) {
         printf("Error opening file\n");
         return 0;
     }
 
     // Read the first line from the file
-    if (!fgets(line1, size1, fp))
-    {
+    if (!fgets(line1, size1, fp)) {
         printf("Error reading the first line from the file\n");
         fclose(fp);
         return 0;
@@ -32,8 +51,7 @@ int readFromFile(const char *filename, char *line1, size_t size1, char *line2, s
         line1[len1 - 1] = '\0';
 
     // Read the second line from the file
-    if (!fgets(line2, size2, fp))
-    {
+    if (!fgets(line2, size2, fp)) {
         printf("Error reading the second line from the file\n");
         fclose(fp);
         return 0;
@@ -47,48 +65,38 @@ int readFromFile(const char *filename, char *line1, size_t size1, char *line2, s
     return 1; // Both lines were successfully read
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     char regexp[100], text[100];
 
     // Check if the user has provided input file name
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Usage: ./a.out filename\n");
         return 1;
     }
 
     // Read the two lines from the file
-    if (!readFromFile(argv[1], regexp, sizeof(regexp), text, sizeof(text)))
-    {
-        //Error already handled in the function
+    if (!readFromFile(argv[1], regexp, sizeof(regexp), text, sizeof(text))) {
+        // Error already handled in the function
         return 1;
     }
 
-    // // Print regexp and text
-    // printf("regexp = %s\n", regexp);
-    // printf("string = %s\n", text);
-
     // Perform regular expression matching
-    if (match(regexp, text))
-    {
+    if (match(regexp, text)) {
         printf("\n");
-    }
-    else
-    {
+    } else {
         printf("no match\n");
     }
 
     return 0;
 }
 
-/* Rob Pike's Regular Expression Matcher with multi-position capabilities*/
+/* Rob Pike's Regular Expression Matcher with multi-position capabilities and extra meta-characters */
 
 /* match: search for regexp anywhere in text and print the positions */
 int match(char *regexp, char *text) {
-    int found = 0;  // Flag to indicate if a match is found
-    int position = 0;  // Position counter
-    int longest_match = 0;  // Store the length of the longest match
+    int found = 0;         // Flag to indicate if a match is found
+    int position = 0;      // Position counter
+    int longest_match = 0; // Store the length of the longest match
 
     if (regexp[0] == '^')
         return matchhere(regexp + 1, text);
@@ -98,11 +106,11 @@ int match(char *regexp, char *text) {
             if (found) {
                 printf(" ");
             } else {
-                found = 1;  // Set the found flag
+                found = 1; // Set the found flag
                 printf("match ");
             }
             printf("%d", position);
-            longest_match = 1;  // Set the longest_match flag
+            longest_match = 1; // Set the longest_match flag
 
             // Skip to the end of the current match to avoid overlaps
             while (text[longest_match] != '\0' && matchhere(regexp, text + longest_match)) {
@@ -121,11 +129,10 @@ int match(char *regexp, char *text) {
 }
 
 /* matchhere: search for regexp at the beginning of text */
-int matchhere(char *regexp, char *text)
-{
+int matchhere(char *regexp, char *text) {
     if (regexp[0] == '\0')
         return 1;
-    if (regexp[1] == '?') 
+    if (regexp[1] == '?')
         return matchquestion(regexp[0], regexp + 2, text);
     if (regexp[1] == '*')
         return matchstar(regexp[0], regexp + 2, text);
@@ -157,8 +164,7 @@ int matchhere(char *regexp, char *text)
 }
 
 /* matchstar: search for c*regexp at the beginning of text */
-int matchstar(int c, char *regexp, char *text)
-{
+int matchstar(int c, char *regexp, char *text) {
     while (*text != '\0' && (*text == c || c == '.')) {
         if (matchhere(regexp, text)) {
             return 1;
@@ -169,8 +175,7 @@ int matchstar(int c, char *regexp, char *text)
 }
 
 /* matchquestion: search for c?regexp at the beginning of text */
-int matchquestion(char c, char *regexp, char *text)
-{
+int matchquestion(char c, char *regexp, char *text) {
     // Try to match without the previous character first.
     if (matchhere(regexp, text))
         return 1;
@@ -180,9 +185,10 @@ int matchquestion(char c, char *regexp, char *text)
     return 0;
 }
 
+/* matchset: search for a character in the set */
 int matchset(char c, char *regexp, char *text) {
     int negate_set = 0;
-    regexp++;  // Skip '['
+    regexp++; // Skip '['
 
     if (regexp[0] == '^') {
         negate_set = 1;
@@ -193,13 +199,13 @@ int matchset(char c, char *regexp, char *text) {
         if (regexp[1] == '-' && regexp[2] >= regexp[0]) {
             if ((c >= regexp[0] && c <= regexp[2]) ||
                 (negate_set && (c < regexp[0] || c > regexp[2]))) {
-                return 1;  // Match found
+                return 1; // Match found
             }
-            regexp += 3;  // Skip range and dash
+            regexp += 3; // Skip range and dash
         } else {
             if ((c == regexp[0]) ||
                 (negate_set && c != regexp[0])) {
-                return 1;  // Match found
+                return 1; // Match found
             }
             regexp++;
         }
@@ -208,6 +214,7 @@ int matchset(char c, char *regexp, char *text) {
     return 0; // No match in the character set
 }
 
+/* matchplus: search for c+regexp at the beginning of text */
 int matchplus(int c, char *regexp, char *text) {
     if (*text == c) {
         return matchstar(c, regexp, text + 1);

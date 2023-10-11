5c56e104870bb5335e156326f02f68d6f0de6a7e
Ahmed Iqbal <ahmediqbal.dev@gmail.com>
2023-10-11 21:39:34 +0500

    Implement set metacharacter

--- regular-expression-matcher.c
+++ regular-expression-matcher.c
@@ -6,6 +6,7 @@ int match(char *regexp, char *text);
 int matchhere(char *regexp, char *text);
 int matchstar(int c, char *regexp, char *text);
 int matchquestion(char c, char *regexp, char *text);
+int matchset(char c, char *regexp, char *text);
 
 // Function to read two lines from the file, removing the newline character if it exists
 int readFromFile(const char *filename, char *line1, size_t size1, char *line2, size_t size2)
@@ -126,8 +127,18 @@ int matchhere(char *regexp, char *text)
             return 0; // Mismatched escape sequence
         }
     }
-    if (*text != '\0' && (regexp[0] == '.' || regexp[0] == *text))
-        return matchhere(regexp + 1, text + 1);
+    if (*text != '\0') {
+        if (regexp[0] == '.') {
+            return matchhere(regexp + 1, text + 1);
+        } else if (regexp[0] == '[') {
+            // Handle character sets
+            return matchset(text[0], regexp, text);
+        } else {
+            if (regexp[0] == text[0]) {
+                return matchhere(regexp + 1, text + 1);
+            }
+        }
+    }
     return 0;
 }
 
@@ -151,4 +162,32 @@ int matchquestion(char c, char *regexp, char *text)
     if (*text != '\0' && (c == '.' || c == *text))
         return matchhere(regexp, text + 1);
     return 0;
+}
+
+int matchset(char c, char *regexp, char *text) {
+    int negate_set = 0;
+    regexp++;  // Skip '['
+
+    if (regexp[0] == '^') {
+        negate_set = 1;
+        regexp++;
+    }
+
+    while (regexp[0] != '\0' && regexp[0] != ']') {
+        if (regexp[1] == '-' && regexp[2] >= regexp[0]) {
+            if ((c >= regexp[0] && c <= regexp[2]) ||
+                (negate_set && (c < regexp[0] || c > regexp[2]))) {
+                return 1;  // Match found
+            }
+            regexp += 3;  // Skip range and dash
+        } else {
+            if ((c == regexp[0]) ||
+                (negate_set && c != regexp[0])) {
+                return 1;  // Match found
+            }
+            regexp++;
+        }
+    }
+
+    return 0; // No match in the character set
 }
